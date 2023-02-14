26d3ed08505cabbdaf9f8cc38c3b875743de71de
$Cuentas-AppServices <antjesben@gmail.com>
2023-02-14 08:55:25 +0000

    Swaping google_project_iam_binding resources for google_project_iam_member as they were overriding permissions of other SA

--- main.tf
+++ main.tf
@@ -8,13 +8,10 @@ resource "google_service_account" "default" {
   display_name = "Accounting WebApp Engine App"
 }
 
-resource "google_project_iam_binding" "sql_client" {
+resource "google_project_iam_member" "sql_client" {
   project = var.project_id
   role    = "roles/cloudsql.client"
-
-  members = [
-    "serviceAccount:${google_service_account.default.email}",
-  ]
+  member  = "serviceAccount:${google_service_account.default.email}"
 }
 
 resource "google_project_iam_member" "gae_api" {
